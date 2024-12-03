c4c90a7e64666f98d86ea7136e1942593bee101a
ashiqur rahman biplop <biplops22@gmail.com>
2024-12-03 16:34:12 +0600

    added som eocde

---
+++ script.js
@@ -0,0 +1,58 @@
+// Access the testimonials
+let testSlide = document.querySelectorAll(".testItem");
+// Access the indicators
+let dots = document.querySelectorAll(".dot");
+const slideYear = document.getElementsByClassName("testi-year");
+
+var counter = 0;
+var deleteInterval;
+
+// Add click event to the indicators
+function switchTest(currentTest) {
+  currentTest.classList.add("active");
+  var testId = currentTest.getAttribute("attr");
+  if (testId > counter) {
+    testSlide[counter].style.animation = "next1 0.5s ease-in forwards";
+    counter = testId;
+    testSlide[counter].style.animation = "next2 0.5s ease-in forwards";
+  } else if (testId == counter) {
+    return;
+  } else {
+    testSlide[counter].style.animation = "prev1 0.5s ease-in forwards";
+    counter = testId;
+    testSlide[counter].style.animation = "prev2 0.5s ease-in forwards";
+  }
+  indicators();
+}
+
+// Add and remove active class from the indicators
+function indicators() {
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[counter].className += " active";
+}
+
+// Code for auto sliding
+function slideNext() {
+  testSlide[counter].style.animation = "next1 0.5s ease-in forwards";
+  if (counter >= testSlide.length - 1) {
+    counter = 0;
+  } else {
+    counter++;
+  }
+  testSlide[counter].style.animation = "next2 0.5s ease-in forwards";
+  indicators();
+}
+
+// Stop auto sliding when mouse is over the indicators
+const container = document.querySelector(".indicators");
+container.addEventListener("mouseover", pause);
+function pause() {
+  clearInterval(deleteInterval);
+}
+
+// Stop auto sliding when mouse is over the testimonials
+testSlide.forEach(function (item) {
+  item.addEventListener("mouseover", pause);
+});
